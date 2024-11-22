8e8443343c4e150b4947a99c76ace94b848b56f6
ThirdWinter <zzx15601035637@outlook.com>
2024-11-22 16:41:29 +0800

    update: add clang-format for *.c

--- nvim/lua/plugins/utils/autoformat.lua
+++ nvim/lua/plugins/utils/autoformat.lua
@@ -35,6 +35,7 @@ return { -- Autoformat
       yaml = { 'yamlfmt' },
       go = { 'gofumpt' },
       xml = { 'xmlformatter' },
+      c = { 'clang-format' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
