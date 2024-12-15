4b6125e34163dc9b6b86b470cdeddd35a91e6104
ThirdWinter <zzx15601035637@outlook.com>
2024-12-15 14:48:48 +0800

    update: update swww scripts

--- waybar/scripts/set_swww_wallpaper.sh
+++ waybar/scripts/set_swww_wallpaper.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
+directory=$HOME/Pictures/wallpapers
 
-set_wallpaper_hyprland() {
-	dir="$HOME/Pictures/wallpapers"
-	BG="$(find "$dir" -name '*.jpg' -o -name '*.png' | shuf -n1)"
-	PROGRAM="swww-daemon"
-	trans_type="grow"
+#check exist
+if [ ! -d "$directory" ]; then
+	notify-send -u critical "Failed!" "no such directory found"
+fi
 
-	if pgrep "$PROGRAM" >/dev/null; then
-		swww img "$BG" --transition-fps 244 --transition-type $trans_type --transition-duration 1 --transition-pos top-right
-	else
-		swww init && swww img "$BG" --transition-fps 244 --transition-type $trans_type --transition-duration 1 --transition-pos top-right
+#get wallpapers
+files=()
+while IFS= read -r -d '' file; do
+	files+=("$file")
+done< <(find "$directory" -type f -print0)
 
+#get the number of wallpapers
+num_files="${#files[@]}"
+
+#check empty
+if [ "$num_files" -eq 1 ]; then
+	notify-send -u critical "Failed!" "no wallpaper to switch"
+fi
+
+#get current wallpaper
+cur_file=$(<"$HOME/.cache/swww/eDP-1")
+
+#change wallpaper use swww
+for ((i=0; i<num_files; i++)); do
+	if [ "${files[$i]}" == "$cur_file" ]; then
+		if ((i == num_files-1)); then
+			swww img "${files[0]}" --transition-fps 80 --transition-type grow --transition-pos top-right --transition-duration 1.3 --transition-bezier 0.43,1.19,1,0.4
+		else
+			swww img "${files[i+1]}" --transition-fps 80 --transition-type grow --transition-pos top-right --transition-duration 1.3 --transition-bezier 0.43,1.19,1,0.4
+		fi
 	fi
-}
-
-set_wallpaper_hyprland
+done
