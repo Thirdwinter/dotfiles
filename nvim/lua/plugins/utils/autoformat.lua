92ff4878316522a758648f37f4ca371f9e066e4d
ThirdWinter <zzx15601035637@outlook.com>
2024-12-24 23:50:20 +0800

    update: update nvim format

--- nvim/lua/plugins/utils/autoformat.lua
+++ nvim/lua/plugins/utils/autoformat.lua
@@ -18,13 +18,13 @@ return { -- Autoformat
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      -- local disable_filetypes = { c = true, cpp = true }
       local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
-      else
-        lsp_format_opt = 'fallback'
-      end
+      -- if disable_filetypes[vim.bo[bufnr].filetype] then
+      --   lsp_format_opt = 'never'
+      -- else
+      --   lsp_format_opt = 'fallback'
+      -- end
       return {
         timeout_ms = 500,
         lsp_format = lsp_format_opt,
@@ -36,7 +36,10 @@ return { -- Autoformat
       go = { 'gofumpt' },
       xml = { 'xmlformatter' },
       c = { 'clang-format' },
+      cpp = { 'clang-format' },
       json = { 'cland-format' },
+      zsh = { 'beautysh' },
+      sh = { 'beautysh' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
