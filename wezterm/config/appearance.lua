9841c06455dca95f61e3d0496f7b842b58bb5928
ThirdWinter <zzx15601035637@outlook.com>
2025-01-04 13:42:18 +0800

    update: change style

--- wezterm/config/appearance.lua
+++ wezterm/config/appearance.lua
@@ -40,14 +40,15 @@ return {
 
    -- tab bar
    enable_tab_bar = true,
-   -- hide_tab_bar_if_only_one_tab = true,
+   hide_tab_bar_if_only_one_tab = true,
    tab_max_width = 25,
    show_tab_index_in_tab_bar = true,
    use_fancy_tab_bar = true,
    -- show_close_tab_button_in_tabs = false,
 
    -- window
-   window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
+   -- window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
+   window_decorations = 'NONE',
    -- integrated_title_button_style = 'Windows',
    integrated_title_button_color = 'auto',
    integrated_title_button_alignment = 'Right',
