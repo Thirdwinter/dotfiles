62ba4c18458357e514c5cf7a6149e58f044938f3
ThirdWinter <zzx15601035637@outlook.com>
2025-01-06 14:21:33 +0800

    feat: clean hypr config like module

---
+++ hypr/Scripts/tmux_0_session.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 检查是否有正在运行的 Kitty 终端
+if pgrep -x "kitty" > /dev/null; then
+  # 使用 overlay 类型启动一个新的 Kitty 终端并检查 tmux 会话
+  bash kitty @ launch --self --type=overlay bash -c "tmux has-session -t 0 2>/dev/null; if [ \$? != 0 ]; then tmux new-session -s 0; else tmux attach-session -t 0; fi"
+else
+  # 启动一个新的 Kitty 终端并检查 tmux 会话
+  kitty bash -c "tmux has-session -t 0 2>/dev/null; if [ \$? != 0 ]; then tmux new-session -s 0; else tmux attach-session -t 0; fi"
+fi
