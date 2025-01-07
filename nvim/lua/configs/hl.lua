aea8b789835c4d3a6a255b8500c695ada5912238
ThirdWinter <zzx15601035637@outlook.com>
2025-01-07 22:45:03 +0800

    fix: fix highlight for lsp

--- nvim/lua/configs/hl.lua
+++ nvim/lua/configs/hl.lua
@@ -2,12 +2,13 @@
 
 vim.cmd.colorscheme 'catppuccin-mocha' ---@type 'monet'|'tokyonight'|'nightly'|'yash'|'everforest'|'newpaper'|'nightfox'|'terafox'|'dracula'|'dracula-soft'
 -- Hide semantic highlights for functions
-vim.api.nvim_set_hl(0, '@lsp.type.function', {})
+-- vim.api.nvim_set_hl(0, '@lsp.type.function', {})
 
+-- vim.api.nvim_set_hl(0, 'CmpCursorLine', { bg = '#a6e3a1', fg = '#1e1e2e', bold = true })
 -- Hide all semantic highlights
-for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
-  vim.api.nvim_set_hl(0, group, {})
-end
+-- for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
+--   vim.api.nvim_set_hl(0, group, {})
+-- end
 
 vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#4F536D' })
 vim.api.nvim_set_hl(0, 'Visual', { bg = '#138376' })
