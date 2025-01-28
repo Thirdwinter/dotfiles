3150989f218ed920fc56667faded46e66efbef66
ThirdWinter <zzx15601035637@outlook.com>
2025-01-28 15:31:05 +0800

    update: change rofi style && remove unused config

--- rofi/launcher.sh
+++
@@ -1,1 +0,0 @@
-rofi -show drun -theme $HOME/.config/rofi/launchers/type-1/style-7.rasi
--- rofi/launchers/type-1/window.sh
+++
@@ -1,7 +0,0 @@
-dir="$HOME/.config/rofi/launchers/type-1"
-theme='style-7'
-
-## Run
-rofi \
-    -show window \
-    -theme ${dir}/${theme}.rasi
