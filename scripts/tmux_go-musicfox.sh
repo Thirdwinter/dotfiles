df5ffdff636931e9432901999e4092d51de5bb91
thirdwinter <thirdwinters@gmail.com>
2025-07-27 09:12:40 +0800

    update script && waybar style

--- scripts/tmux_go-musicfox.sh
+++ scripts/tmux_go-musicfox.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# 使用Kitty启动Tmux，并创建一个新的会话（如果不存在），然后在该会话中执行指定命令
 
 SESSION_NAME="music"
 COMMAND="musicfox"
 
-# 使用Kitty启动Tmux，并创建一个新的会话或附加到现有会话
-kitty --class "go-musicfox" tmux new-session -A -s $SESSION_NAME $COMMAND
+kitty --class "go-musicfox" tmux new-session -A -s $SESSION_NAME $COMMAND \
+    \; split-window -v \
+    \; resize-pane -y 30% \
+    \; send-keys 'cava' Enter \
+    \; select-pane -t '{top}'
